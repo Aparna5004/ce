9b616b391f4efd5ce1746964c7e62401e314ddc8
Aparna5004 <apriyadarshani925@gmail.com>
2023-01-07 12:24:03 +0530

    <hello world>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HelppageComponent } from './helppage/helppage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CyviewComponent } from './cyview/cyview.component';
+import { FilterpageComponent } from './filterpage/filterpage.component';
+import { AccountSettingComponent } from './account-setting/account-setting.component';
+const routes: Routes = [
+  { path: "sidenav", component: SidenavComponent },
+  { path: "helppage", component: HelppageComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "cyview", component: CyviewComponent },
+  { path: "filterpage", component: FilterpageComponent },
+  { path: "account-setting", component: AccountSettingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HelppageComponent } from './helppage/helppage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CyviewComponent } from './cyview/cyview.component';
+import { FilterpageComponent } from './filterpage/filterpage.component';
+import { AccountSettingComponent } from './account-setting/account-setting.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HelppageComponent,
+    DashboardComponent,
+    CyviewComponent,
+    FilterpageComponent,
+    AccountSettingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FormsModule,
+    MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
